4aec7323e4651a21674914ba26afd3c5948934b3
Egemen Gunduz <gndzegemen@gmail.com>
2025-04-22 17:54:24 +0300

    exercise 1 is finished

--- core/src/main/java/io/github/test042225/Main.java
+++ core/src/main/java/io/github/test042225/Main.java
@@ -11,18 +11,29 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private float startingPoint;
 
     @Override
     public void create() {
+        startingPoint = 50;
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
     }
 
     @Override
     public void render() {
+        draw();
+        logic();
+    }
+
+    public void logic(){
+        startingPoint += 100 * Gdx.graphics.getDeltaTime();
+    }
+
+    public void draw(){
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(image, startingPoint, 0);
         batch.end();
     }
 
